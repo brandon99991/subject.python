31a83ecbccf61424c93c84beb62813c6533f064c
note15 <test@test.com>
2025-03-28 21:45:24 +0900

    create range_test.py

---
+++ week04/range_test.py
@@ -0,0 +1,19 @@
+for i in range(5):
+    print(i)
+print('-----------------------------------')
+
+for i in range(2, 7):
+    print(i)
+print('-----------------------------------')
+
+for i in range(1, 10, 2):
+    print(i)
+print('-----------------------------------')
+
+for i in range(10, 0, -1):
+    print(i)
+print('-----------------------------------')
+
+numbers = list(range(5))
+print(numbers)
+print('-----------------------------------')
