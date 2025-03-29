a25b38165995681b0dba70949b19dc000dd644e3
note15 <test@test.com>
2025-03-29 10:41:34 +0900

    update

---
+++ week04/FunctionList02_3.py
@@ -0,0 +1,18 @@
+###################################
+# 파이썬 print함수 사용법
+# print함수 : 기본 사용법
+# print함수 : f옵션을 이용한 방법
+###################################
+
+country = "Korea"
+print("welcome to ", country)
+print("=====================================")
+
+size = 10
+print("My Size ", size)
+print("=====================================")
+
+name = "Alice"
+age = 25
+print(f"My name is {name} and I am {age} years old.")
+print("=====================================")
